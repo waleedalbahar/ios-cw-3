b9fd6033cf9d63e34f0eba729004f91e92eca127
waleedalbahar <waleedalbahar10@gmail.com>
2020-06-20 20:54:34 +0300

    waleed

--- Classwork3iOS/Classwork3iOS/ViewController.swift
+++ Classwork3iOS/Classwork3iOS/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         
         
         // MARK: -  قم باستدعاء الداله هنا
-        
+        addName(name: name, helloArray: helloLangauge, flagArray: flagLanguage)
         
         // MARK: -  النهايه⚠️⚠️لا تقم بغير تحت هذا السطر
         
@@ -47,7 +47,12 @@ class ViewController: UIViewController {
     
     
     // MARK: -  قم بكتابة الداله هنا
+    func addName(name: String, helloArray:  [String], flagArray: [String]){
+        for i in 0..<helloArray.count{
+            greetingArray[i] =  helloArray[i] + " " + name + " " + flagArray[i]
     
+        }
+    }
     
     
     // MARK: -   النهايه⚠️⚠️ لا تقوم بغير تحت هذا السطر
